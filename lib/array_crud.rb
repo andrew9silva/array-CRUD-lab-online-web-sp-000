921107ebfa1745562b0b029a021501cf6678d9c7
Andrew Silva <andrewsilva9@gmail.com>
2019-04-21 16:52:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,6 +33,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+  red_sox = ["mookie", "benintendi", "jd", "sale"]
+  red_sox[0]
 
 end
 
